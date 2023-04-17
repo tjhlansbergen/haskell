65e7b1157f48823a094482733f66a31b30d71d01
Tako Lansbergen <takolansbergen@xs4all.nl>
2023-04-17 21:00:17 +0200

    Add video tutorial file

---
+++ videotut.hs
@@ -0,0 +1,15 @@
+-- Haskell Tutorial by Derek Banas
+-- https://www.youtube.com/watch?v=02_H3LjqMr8
+
+{- 
+multiline comment
+-}
+
+import Data.List
+import System.IO
+
+maxInt = maxBound :: Int
+
+sumOfNums = sum [1..1000]
+modEx = mod 5 4
+modEx2 = 5 `mod` 4
